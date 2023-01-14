3d412518f45219ff899e0c052ddd3494128700a5
xueQianBanQ <115210866+xueQianBanQ@users.noreply.github.com>
2023-01-14 20:40:01 +0800

    Add files via upload

---
+++ online_1_14.cpp
@@ -0,0 +1,51 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//	cout << "hello world" << endl;
+//	return 0;
+//}
+
+
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//	float f1 = 0.1415967f;
+//	cout << "f1 = " << f1 << endl;
+//	float f2 = 3.14159886;
+//	cout << "f2 = " << f2 << endl;
+//	return 0;
+//}
+
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//int main()
+//{
+//	string str2 = "hello world";
+//	cout << str2 << endl;
+//	return 0;
+//}
+
+//
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//	bool flag = false;
+//	cout << "please assign a value of bool to flag" << endl;
+//	cin >> flag;
+//	cout << "flag = " << flag << endl;
+//	return 0;
+//}
+
+#include <iostream> 
+using namespace std;
+int main()
+{
+	int a = 10 / 3;
+	cout << a << endl;
+	return 0;
+}
