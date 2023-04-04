da171438d7cbb3990986de509244f0ace6a7d097
xueQianBanQ <115210866+xueQianBanQ@users.noreply.github.com>
2023-04-04 22:25:34 +0800

    Add files via upload

--- test.c
+++ test.c
@@ -1,6 +1,7 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
 typedef int BTDataType;
 typedef struct BinaryTreeNode
 {
@@ -34,7 +35,6 @@ BTNode* CreateTree()
 	BTNode* node4 = BuyNode(4);
 	BTNode* node5 = BuyNode(5);
 	BTNode* node6 = BuyNode(6);
-	BTNode* node7 = BuyNode(7);
 
 	node1->left = node2;
 	node1->right = node4;
@@ -81,13 +81,53 @@ void PostOrder(BTNode* root)
 	printf("%d ", root->data);
 }
 
+int TreeSize(BTNode* root)
+{
+	return root == NULL ? 0 : TreeSize(root->left)
+		+ TreeSize(root->right) + 1;
+}
+
+int TreeHeight(BTNode* root)
+{
+	if (root == NULL)
+	{
+		return 0;
+	}
+	int left = TreeHeight(root->left);
+	int right = TreeHeight(root->right);
+	return left > right ? left + 1 : right + 1;
+}
+
+int TreeKLevel(BTNode* root, int k)
+{
+	assert(k > 0);
+	if (root == NULL)
+	{
+		return 0;
+	}
+	else if(k == 1)
+	{
+		return 1;
+	}
+	return TreeKLevel(root->left, k - 1) + TreeKLevel(root->right, k - 1);
+}
+
 int main()
 {
 	BTNode* root = CreateTree();
-	PreOrder(root);
+	/*PreOrder(root);
 	printf("\n");
 	InOrder(root);
 	printf("\n");
-	PostOrder(root);
+	PostOrder(root);*/
+
+	int n = TreeKLevel(root, 3);
+	printf("%d\n", n);
+
+	int height = TreeHeight(root);
+	printf("%d\n", height);
+
+	int size = TreeSize(root);
+	printf("%d\n", size);
 	return 0;
 }
